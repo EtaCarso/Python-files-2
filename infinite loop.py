66aabc09dbda535e1682e8ad3c543fa70c928c5c
ETA CARSO <118751175+EtaCarso@users.noreply.github.com>
2022-12-26 02:06:46 +0100

    Add files via upload

---
+++ infinite loop.py
@@ -0,0 +1,5 @@
+#while the statement is true, the loop will continue increasing by one number until infinity. or until we stop it manually from running
+list = 0
+while True:
+    print(list)
+    list += 1
